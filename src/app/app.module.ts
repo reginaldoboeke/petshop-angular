e5327993c4654a6dbdf0338f593e03155c381824
Reginaldo Boeke <reginaldoboeke@gmail.com>
2020-04-12 21:36:32 -0300

    Montado um protótipo da interface do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent,
+    PetsPageComponent,
+    ProductsPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
